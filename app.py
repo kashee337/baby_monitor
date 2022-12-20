829e57f8d95a87546502e49518e136bdeccdc43e
klee <k.r.okayama@gmail.com>
2022-12-20 22:39:39 +0900

    add PoseBase NegeariDetection

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import statistics
 import time
 from datetime import datetime
 
 import cv2
-import mediapipe as mp
 import streamlit as st
+
+from negaeri_detector import FaceBase, NegaeriDetector, PoseBase
 
 # camera setting
 cap = cv2.VideoCapture(0)
@@ -21,46 +21,12 @@ def check_refresh(refresh_minute_rate=1):
     return minute % refresh_minute_rate == 0 and second == 0
 
 
-class NegaeriDetector:
-
-    def __init__(self, window_size=5, is_draw=True):
-        # face detector
-        mp_face_detection = mp.solutions.face_detection
-        self.face_detection = mp_face_detection.FaceDetection(
-            model_selection=1, min_detection_confidence=0.5)
-        self.is_draw = is_draw
-        if is_draw:
-            self.mp_drawing = mp.solutions.drawing_utils
-
-        # for filtering
-        self.window = [False for _ in range(window_size)]
-        self.idx = 0
-
-    def __del__(self):
-        self.face_detection.close()
-
-    def __call__(self, img):
-
-        results = self.face_detection.process(img)
-
-        if results.detections:
-            self.window[self.idx] = True
-            if self.is_draw:
-                for detection in results.detections:
-                    self.mp_drawing.draw_detection(img, detection)
-        else:
-            self.window[self.idx] = False
-
-        self.idx += 1
-        self.idx %= len(self.window)
-
-        return statistics.mode(self.window)
-
-
-negaeri_detector = NegaeriDetector()
+is_ir = True
+# detector = FaceBase(is_ir)
+detector = PoseBase(is_ir)
+negaeri_detector = NegaeriDetector(detector)
 
 st.title('Baby Monitor')
-
 # place holder
 baby_status = st.empty()
 monitor_canvas = st.empty()
@@ -87,6 +53,7 @@ while cap.isOpened:
                     color=(0, 255, 0),
                     thickness=1,
                     lineType=cv2.LINE_4)
+
         monitor_canvas.image(img)
     else:
         print("missing")
---
+++ negaeri_detector.py
@@ -0,0 +1,122 @@
+import statistics
+from abc import ABC, abstractmethod
+
+import numpy as np
+import cv2
+import mediapipe as mp
+
+
+class Detector(ABC):
+
+    def __init__(self, is_ir=False):
+        pass
+
+    @abstractmethod
+    def detect(self, img):
+        pass
+
+    @abstractmethod
+    def draw(self, img):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
+
+class PoseBase(Detector):
+
+    def __init__(self, is_ir=False):
+        # pose detector
+        self.mp_pose = mp.solutions.pose
+        self.pose_detector = self.mp_pose.Pose(
+            min_detection_confidence=0.3,
+            min_tracking_confidence=0.5)
+
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_drawing_styles = mp.solutions.drawing_styles
+
+        self.is_ir = is_ir
+
+    def detect(self, img):
+        if self.is_ir:
+            gray_img = np.stack(
+                (cv2.cvtColor(img, cv2.COLOR_RGB2GRAY),)*3, axis=-1)
+            results = self.pose_detector.process(gray_img)
+        else:
+            results = self.pose_detector.process(img)
+        ret = results.pose_landmarks is not None
+        return ret, results
+
+    def draw(self, img, results):
+        self.mp_drawing.draw_landmarks(
+            img,
+            results.pose_landmarks,
+            self.mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=self.mp_drawing_styles.get_default_pose_landmarks_style())
+
+    def close(self):
+        self.pose_detector.close()
+
+
+class FaceBase(Detector):
+
+    def __init__(self, is_ir=False):
+        # face detector
+        mp_face_detection = mp.solutions.face_detection
+        self.face_detection = mp_face_detection.FaceDetection(
+            model_selection=1, min_detection_confidence=0.5)
+
+        self.mp_drawing = mp.solutions.drawing_utils
+
+        self.is_ir = is_ir
+
+    def detect(self, img):
+
+        if self.is_ir:
+            gray_img = np.stack(
+                (cv2.cvtColor(img, cv2.COLOR_RGB2GRAY),)*3, axis=-1)
+            results = self.face_detection.process(gray_img)
+        else:
+            results = self.face_detection.process(img)
+
+        ret = results.detections is not None
+        return ret, results
+
+    def draw(self, img, results):
+        for detection in results.detections:
+            self.mp_drawing.draw_detection(img, detection)
+
+    def close(self):
+        self.face_detctor.close()
+
+
+class NegaeriDetector:
+
+    def __init__(self, detector, window_size=5, is_draw=True):
+
+        self.detector = detector
+        self.is_draw = is_draw
+
+        # for filtering
+        self.window = [False for _ in range(window_size)]
+        self.idx = 0
+
+    def __del__(self):
+        self.detector.close()
+
+    def __call__(self, img):
+
+        ret, results = self.detector.detect(img)
+
+        if ret:
+            self.window[self.idx] = True
+            if self.is_draw:
+                self.detector.draw(img, results)
+        else:
+            self.window[self.idx] = False
+
+        self.idx += 1
+        self.idx %= len(self.window)
+
+        return statistics.mode(self.window)
